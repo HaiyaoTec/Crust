bbeba6715c6e936782b590ecac512106b7c01e3c
Jude <973829691@qq.com>
2021-01-16 18:16:31 +0800

    新增text记录动态配置

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,12 @@ import render from 'koa-ejs';
 import * as path from "path";
 import request from "request";
 import htmlparser2 from 'htmlparser2';
-var RuntimeError = WebAssembly.RuntimeError;
+import dns from "dns";
+
 const targetUrl = process.env.TARGET ? process.env.TARGET : "https://baidu.com";
 const countdown = process.env.COUNTDOWN ? process.env.COUNTDOWN : 0;
 const port = process.env.PORT ? process.env.PORT : 80;
-let head;
+
 const app = new Koa();
 render(app, {
     root: path.resolve('template'),
@@ -16,16 +17,53 @@ render(app, {
     cache: true,
     debug: false
 });
-request(targetUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log("Load Page Success");
-        head = parseHead(body);
-    }
-    else {
-        console.log("Load Page Failed");
-        throw new RuntimeError();
-    }
+app.use(async function (ctx) {
+    let requestHead = "";
+    let requestTarget = targetUrl;
+    let requestCountDown = countdown;
+    let requestClickGo = "false";
+    await new Promise(resolve => {
+        dns.resolveTxt("crust." + ctx.hostname, function (err, addresses) {
+            if (!err && addresses) {
+                let argMap = new Map();
+                addresses[0][0].split("&").forEach(arg => {
+                    let [key, value] = arg.split("=");
+                    argMap.set(key, value);
+                });
+                if (argMap.has("target"))
+                    requestTarget = argMap.get("target");
+                if (argMap.has("countDown"))
+                    requestCountDown = argMap.get("countDown");
+                if (argMap.has("clickgo"))
+                    requestClickGo = argMap.get("clickgo");
+            }
+            resolve();
+        });
+    });
+    requestHead = await parseHeadFor(requestTarget);
+    await ctx.render('index', {
+        head: requestHead,
+        target: requestTarget + ctx.request.path + ctx.request.search,
+        countdown: requestCountDown,
+        clickgo: requestClickGo
+    });
 });
+app.listen(port);
+console.log("Start web server at http://localhost:" + port);
+function parseHeadFor(url) {
+    return new Promise(resolve => {
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Load Page Success");
+                resolve(parseHead(body));
+            }
+            else {
+                console.log("Load Page Failed");
+                resolve("");
+            }
+        });
+    });
+}
 function parseHead(html) {
     let content = "";
     let titleReady = false;
@@ -54,12 +92,3 @@ function parseHead(html) {
     parser.end();
     return content;
 }
-app.use(async function (ctx) {
-    await ctx.render('index', {
-        head: head,
-        target: targetUrl + ctx.request.path + ctx.request.search,
-        countdown: countdown
-    });
-});
-app.listen(port);
-console.log("Start web server at http://localhost:" + port);
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import render from 'koa-ejs'
 import * as path from "path";
 import request from "request"
 import htmlparser2 from 'htmlparser2'
-import RuntimeError = WebAssembly.RuntimeError;
-
+import dns from "dns"
 
 const targetUrl = process.env.TARGET ? process.env.TARGET as string : "https://baidu.com"
 const countdown = process.env.COUNTDOWN ? process.env.COUNTDOWN : 0
@@ -21,15 +20,57 @@ render(app, {
     debug: false
 });
 
-request(targetUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200){
-        console.log("Load Page Success")
-        head = parseHead(body)
-    }else {
-        console.log("Load Page Failed")
-        throw new RuntimeError()
-    }
-})
+app.use(async function (ctx) {
+    let requestHead = head
+    let requestTarget = targetUrl
+    let requestCountDown = countdown
+    let requestClickGo = "false"
+
+    await new Promise<void>(resolve => {
+        dns.resolveTxt("crust."+ctx.hostname, function (err, addresses) {
+            if (!err && addresses){
+                console.log(addresses)
+                let argMap = new Map<string,string>()
+                addresses[0][0].split("&").forEach(arg=>{
+                    let [key,value] = arg.split("=")
+                    argMap.set(key, value)
+                })
+                console.log(argMap)
+                if (argMap.has("target")) requestTarget = argMap.get("target") as string
+                if (argMap.has("countDown")) requestCountDown = argMap.get("countDown") as string
+                if (argMap.has("clickgo")) requestClickGo = argMap.get("clickgo") as string
+            }
+            resolve()
+        })
+    })
+
+    requestHead = await parseHeadFor(requestTarget)
+    console.log(requestHead)
+    await ctx.render('index', {
+        head: requestHead,
+        target: requestTarget+ctx.request.path+ctx.request.search,
+        countdown: requestCountDown,
+        clickgo: requestClickGo
+    });
+
+});
+
+app.listen(port)
+console.log("Start web server at http://localhost:"+port)
+
+function parseHeadFor(url: string):Promise<string>{
+    return new Promise(resolve => {
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200){
+                console.log("Load Page Success")
+                resolve(parseHead(body))
+            }else {
+                console.log("Load Page Failed")
+                resolve("")
+            }
+        })
+    })
+}
 
 function parseHead(html: string):string{
     let content: string = ""
@@ -60,15 +101,3 @@ function parseHead(html: string):string{
     parser.end();
     return content
 }
-
-
-app.use(async function (ctx) {
-    await ctx.render('index', {
-        head: head,
-        target: targetUrl+ctx.request.path+ctx.request.search,
-        countdown: countdown
-    });
-});
-
-app.listen(port)
-console.log("Start web server at http://localhost:"+port)
